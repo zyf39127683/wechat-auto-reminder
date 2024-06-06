96222ebf969a098b73b6bb9855a26e551ef730d1
大凡 <155544055+zyf39127683@users.noreply.github.com>
2024-06-06 13:39:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_weather(region):
     }
     key = config["weather_key"]
     region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, key)
-    response = requests.get(region_url, headers=headers).json()
+    response = get(region_url, headers=headers).json()
     if response["code"] == "404":
         print("推送消息失败，请检查地区名是否有误！")
         os.system("pause")
